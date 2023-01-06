ab19af7836c0a05ede6fce8f9e4ee3f02dca3962
Temitope Aiyebogan <aphrotemitope37@gmail.com>
2023-01-06 04:36:19 +0100

    tasks 10, 11 & 12

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,7 +7,8 @@ This module provides the function `safe_first_element`
 import typing
 
 
-def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing.Union[typing.Any, None]:
+def safe_first_element(lst: typing.Sequence[typing.Any])\
+                        -> typing.Union[typing.Any, None]:
     """
     This function returns the first element of
     the input if it exists otherwise None
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,8 +6,14 @@ This module provides the function `safeky_get_value`
 
 import typing
 
+
 T = typing.TypeVar('T')
-def safely_get_value(dct: typing.Mapping[typing.Any, typing.Any], key: typing.Any, default: typing.Union[typing.Any, None] = None) -> typing.Union[T, typing.Any, None]:
+
+
+def safely_get_value(dct: typing.Mapping,
+                     key: typing.Any, default:
+                     typing.Union[T, typing.Any, None] = None)\
+                     -> typing.Union[typing.Any, T]:
     """
     This function returns the value of the key in the dictionary
     """
