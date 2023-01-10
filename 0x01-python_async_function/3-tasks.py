2bface219645f236950c9330b87316b34f574c2e
Temitope Aiyebogan <aphrotemitope37@gmail.com>
2023-01-10 01:40:24 +0100

    task 3 completed

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+This module provides the function `task_wait_random`
+'''
+
+import asyncio
+import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> typing.TypeVar('asyncio.Task'):
+    '''
+    This function  that takes an integer
+    max_delay and returns a asyncio.Task
+    '''
+    return asyncio.create_task(wait_random(max_delay))
