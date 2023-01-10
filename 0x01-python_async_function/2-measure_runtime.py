db884acb92c784aead8e7121d2555f2f540c26c4
Temitope Aiyebogan <aphrotemitope37@gmail.com>
2023-01-10 01:51:08 +0100

    task 4 completed alongside error fixes

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,6 +7,7 @@ import time
 import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     '''
     This function measures the total execution time for
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
 """
-This module provides the function `wait_n`
+This module provides the function `task_wait_n`
 """
 
 import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     """
     This function that takes in 2 int arguments n and max_delay,
-    spawns wait_random n times with the specified max_delay and
+    runs the task task_wait_random with the specified max_delay and
     returns the list of all the delays in ascending order.
     """
     delays: typing.List[float] = []
     delay: float = 0.0
     for i in range(n):
-        delay = await wait_random(max_delay)
+        delay = await task_wait_random(max_delay)
         delays.append(delay)
     delaysT = tuple(delays)
     delays = list(sorted(delaysT))
